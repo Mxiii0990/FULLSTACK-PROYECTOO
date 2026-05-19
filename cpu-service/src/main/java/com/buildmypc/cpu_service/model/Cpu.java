ecc7c115c2b769bf0a96e69d10c29e06d86f4b99
max <max.veas@duocuc.cl>
2026-05-19 11:50:34 -0400

    definir entidad Cpu con mapeo de campos socket, cores, threads y tdp"

---
+++ cpu-service/src/main/java/com/buildmypc/cpu_service/model/Cpu.java
@@ -0,0 +1,20 @@
+package com.buildmypc.cpu_service.model;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "cpus")
+@Data
+public class Cpu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long componentId; // Conecta con el ID del component-service
+
+    private String socket;
+    private Integer cores;
+    private Integer threads;
+    private Integer tdp; // Consumo en Watts
+}
