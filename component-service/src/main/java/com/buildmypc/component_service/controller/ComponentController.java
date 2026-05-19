6bf767c397d75c4cff885096984233486a0f434e
max <max.veas@duocuc.cl>
2026-05-19 11:23:51 -0400

    implementar ComponentController para exponer endpoints REST de consulta y creacion"

--- component-service/src/main/java/com/buildmypc/component_service/controller/ComponentController.java
+++ component-service/src/main/java/com/buildmypc/component_service/controller/ComponentController.java
@@ -1,4 +1,28 @@
 package com.buildmypc.component_service.controller;
+import com.buildmypc.component_service.dto.ComponentRequestDTO;
+import com.buildmypc.component_service.dto.ComponentResponseDTO;
+import com.buildmypc.component_service.service.ComponentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
+@RestController
+@RequestMapping("/api/components")
 public class ComponentController {
+    @Autowired
+    private ComponentService service;
+
+    @GetMapping
+    public ResponseEntity<List<ComponentResponseDTO>> listarComponent() {
+        return ResponseEntity.ok(service.obtenerTodos());
+    }
+
+    @PostMapping
+    public ResponseEntity<ComponentResponseDTO> crearComponent(@Valid @RequestBody ComponentRequestDTO request) {
+        ComponentResponseDTO nuevoComponent = service.crearComponent(request);
+        return new ResponseEntity<>(nuevoComponent, HttpStatus.CREATED);
+    }
 }
