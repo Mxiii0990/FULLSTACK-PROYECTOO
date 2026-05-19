d2483bc2a7ebd79172fe2c90c6e46501c67a1f80
max <max.veas@duocuc.cl>
2026-05-19 11:36:58 -0400

    se crear msvc cpu y se sube

---
+++ cpu-service/src/main/java/com/buildmypc/cpu_service/CpuServiceApplication.java
@@ -0,0 +1,13 @@
+package com.buildmypc.cpu_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpuServiceApplication.class, args);
+	}
+
+}
---
+++ cpu-service/src/test/java/com/buildmypc/cpu_service/CpuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildmypc.cpu_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
