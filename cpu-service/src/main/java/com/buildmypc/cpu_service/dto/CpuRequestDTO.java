19a800d15174f87472829724d9f3ec9a3e446062
max <max.veas@duocuc.cl>
2026-05-19 11:54:38 -0400

    crear CpuRequestDTO con validaciones de campos obligatorios

---
+++ cpu-service/src/main/java/com/buildmypc/cpu_service/dto/CpuRequestDTO.java
@@ -0,0 +1,25 @@
+package com.buildmypc.cpu_service.dto;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class CpuRequestDTO {
+    @NotNull(message = "El ID del componente es obligatorio")
+    private Long componentId;
+
+    @NotBlank(message = "El socket es obligatorio")
+    private String socket;
+
+    @NotNull(message = "Los nucleos son obligatorios")
+    @Min(value = 1, message = "Debe tener minimo 1 nucleo")
+    private Integer cores;
+
+    @NotNull(message = "Los hilos son obligatorios")
+    @Min(value = 1, message = "Debe tener minimo 1 hilo")
+    private Integer threads;
+
+    @NotNull(message = "El TDP es obligatorio")
+    private Integer tdp;
+}
