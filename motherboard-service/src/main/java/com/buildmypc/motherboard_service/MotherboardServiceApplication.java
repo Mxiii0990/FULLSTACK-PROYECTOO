d4aa731bf286b4c6f5d2d9c860d26ea45d2abc80
MAXI <max.veas@duocuc.cl>
2026-05-20 10:18:38 -0400

    se crea MSCV motherboard

---
+++ motherboard-service/src/main/java/com/buildmypc/motherboard_service/MotherboardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.buildmypc.motherboard_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotherboardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotherboardServiceApplication.class, args);
+	}
+
+}
---
+++ motherboard-service/src/test/java/com/buildmypc/motherboard_service/MotherboardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildmypc.motherboard_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotherboardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
