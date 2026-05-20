3ebd0017d321e41e4b5b49ef48934216853a6261
dylan <dyl.navarrete@duocuc.cl>
2026-05-20 14:24:26 -0400

    se creo msvc compatibility

---
+++ compatibility-service/src/main/java/com/buildmypc/compatibility_service/CompatibilityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.buildmypc.compatibility_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompatibilityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompatibilityServiceApplication.class, args);
+	}
+
+}
