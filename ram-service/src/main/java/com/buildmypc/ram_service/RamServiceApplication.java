e92a399b9ceeef40b6c17e122c577b70ea8e63b5
MAXI <max.veas@duocuc.cl>
2026-05-20 10:10:11 -0400

    se crea microservicio ram-service (nombre corregido)

---
+++ ram-service/src/main/java/com/buildmypc/ram_service/RamServiceApplication.java
@@ -0,0 +1,13 @@
+package com.buildmypc.ram_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamServiceApplication.class, args);
+	}
+
+}
