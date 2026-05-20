f9b9cefd1b3ff82331e36d8e4c3472fd975a676d
MAXI <max.veas@duocuc.cl>
2026-05-20 10:09:40 -0400

    SE BORRO MICROSERVICIO RAM.SERVICE YA QUE HUBO ERROR EN EL NOMBRE

--- ram.service/src/main/java/com/buildmypc/ram/service/repository/RamRepository.java
+++
@@ -1,8 +0,0 @@
-package com.buildmypc.ram.service.repository;
-import com.buildmypc.ram.service.model.Ram;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RamRepository extends JpaRepository<Ram, Long>{
-}
