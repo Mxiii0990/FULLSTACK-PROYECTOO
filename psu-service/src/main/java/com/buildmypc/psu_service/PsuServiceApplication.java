eebb3801b60b96a4680ca832cb0fad023d7cc6bd
MAXI <max.veas@duocuc.cl>
2026-05-20 11:48:07 -0400

    se crea msvc psu

---
+++ psu-service/src/main/java/com/buildmypc/psu_service/PsuServiceApplication.java
@@ -0,0 +1,13 @@
+package com.buildmypc.psu_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsuServiceApplication.class, args);
+	}
+
+}
---
+++ psu-service/src/test/java/com/buildmypc/psu_service/PsuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildmypc.psu_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
