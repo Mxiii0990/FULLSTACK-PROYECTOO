99983783983075f9a00ddd3c6ba7fb18ab261477
MAXI <max.veas@duocuc.cl>
2026-05-20 10:31:13 -0400

    se crea msvc gpu

---
+++ gpu-service/src/main/java/com/buildmypc/gpu_service/GpuServiceApplication.java
@@ -0,0 +1,13 @@
+package com.buildmypc.gpu_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpuServiceApplication.class, args);
+	}
+
+}
---
+++ gpu-service/src/test/java/com/buildmypc/gpu_service/GpuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildmypc.gpu_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
