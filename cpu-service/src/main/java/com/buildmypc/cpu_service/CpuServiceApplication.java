61b30ec48c6cfd5a6966d5084f61213d4df76895
maxi <max.veas@duocuc.cl>
2026-05-22 11:31:15 -0400

    habilitamos la anotacion de OpenFeign en la clase principal

--- cpu-service/src/main/java/com/buildmypc/cpu_service/CpuServiceApplication.java
+++ cpu-service/src/main/java/com/buildmypc/cpu_service/CpuServiceApplication.java
@@ -2,12 +2,12 @@ package com.buildmypc.cpu_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients; // Importante
 
 @SpringBootApplication
+@EnableFeignClients
 public class CpuServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CpuServiceApplication.class, args);
 	}
-
 }
