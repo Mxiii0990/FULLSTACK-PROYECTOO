3fdb79fcd14cadecb557ada30de62d77841ca616
maxi <max.veas@duocuc.cl>
2026-05-22 11:39:33 -0400

    habilitar OpenFeign en el servicio de placas madres

--- motherboard-service/src/main/java/com/buildmypc/motherboard_service/MotherboardServiceApplication.java
+++ motherboard-service/src/main/java/com/buildmypc/motherboard_service/MotherboardServiceApplication.java
@@ -2,8 +2,9 @@ package com.buildmypc.motherboard_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
+@EnableFeignClients
 public class MotherboardServiceApplication {
 
 	public static void main(String[] args) {
