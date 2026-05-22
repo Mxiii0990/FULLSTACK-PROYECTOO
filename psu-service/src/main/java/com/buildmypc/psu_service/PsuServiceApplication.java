44eb6f4d85fca9b6ef69e069926b1236d8283ab4
maxi <max.veas@duocuc.cl>
2026-05-22 12:02:23 -0400

    habilitar OpenFeign en el servicio de fuentes de poder

--- psu-service/src/main/java/com/buildmypc/psu_service/PsuServiceApplication.java
+++ psu-service/src/main/java/com/buildmypc/psu_service/PsuServiceApplication.java
@@ -1,9 +1,9 @@
 package com.buildmypc.psu_service;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
+@EnableFeignClients
 public class PsuServiceApplication {
 
 	public static void main(String[] args) {
