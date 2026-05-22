a6b6bd3ef0246148bd60be9e2b9a9ec3db04271a
maxi <max.veas@duocuc.cl>
2026-05-22 11:54:14 -0400

    crear cliente de comunicacion hacia el catalogo de componentes

---
+++ ram-service/src/main/java/com/buildmypc/ram_service/client/ComponentClient.java
@@ -0,0 +1,12 @@
+package com.buildmypc.ram_service.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "component-service", url = "http://localhost:8081/api/components")
+public interface ComponentClient {
+
+    @GetMapping("/{id}")
+    Object obtenerComponentePorId(@PathVariable("id") Long id);
+}
