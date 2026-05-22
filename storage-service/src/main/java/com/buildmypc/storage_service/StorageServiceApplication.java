4039a14efdba45ae0ba8c30e591cad9bc13ec500
maxi <max.veas@duocuc.cl>
2026-05-22 12:03:03 -0400

    habilitar OpenFeign en el servicio de almacenamiento

--- storage-service/src/main/java/com/buildmypc/storage_service/StorageServiceApplication.java
+++ storage-service/src/main/java/com/buildmypc/storage_service/StorageServiceApplication.java
@@ -1,9 +1,9 @@
 package com.buildmypc.storage_service;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
+@EnableFeignClients
 public class StorageServiceApplication {
 
 	public static void main(String[] args) {
