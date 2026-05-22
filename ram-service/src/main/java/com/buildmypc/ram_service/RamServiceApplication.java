1579cba5d4b1c60b3381a5360aed3780518a58b8
maxi <max.veas@duocuc.cl>
2026-05-22 11:54:03 -0400

    habilitar OpenFeign en el servicio de memoria RAM

--- ram-service/src/main/java/com/buildmypc/ram_service/RamServiceApplication.java
+++ ram-service/src/main/java/com/buildmypc/ram_service/RamServiceApplication.java
@@ -1,9 +1,9 @@
 package com.buildmypc.ram_service;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
+@EnableFeignClients
 public class RamServiceApplication {
 
 	public static void main(String[] args) {
